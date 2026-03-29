baffd69c4713131ca3d878ba299367537891bcdc
poongothai7190 <poongothai.ece@gmail.com>
2026-03-28 20:00:54 -0700

    Wrong url to make one TC fail for Jenkins Allure report

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -17,7 +17,8 @@ test("@critical Sample test with annotation", async ({ page }) => {
   allure.severity("critical"); // <-- THIS sets severity in Allure reports
   //test.step() -> Step description → helps in Allure reports / debugging
   await test.step("Open application", async () => {
-    await page.goto("http://49.249.28.218:8098/");
+    // await page.goto("http://49.249.28.218:8098/");
+    await page.goto("http://49.249.28.218:809/");
   });
   await test.step("Validate title", async () => {
     const title = await page.title();
